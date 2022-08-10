88fb2628879353bd21a2e089f4eecb563ae56e30
Sergio Insaurralde <srinsaurralde@gmail.com>
2022-08-09 22:31:26 -0300

    Pre-deployment Vervel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,24 @@
+
+import Head from 'next/head'
+import { FC, PropsWithChildren } from 'react'
+import { Navbar } from '../Navbar'
+import styles from './MainLayout.module.css'
+
+
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Sergio</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+        { children }
+      </main>
+    </div>
+  )
+}
